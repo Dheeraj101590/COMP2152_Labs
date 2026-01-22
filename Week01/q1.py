96a21563fbcabb937a9709ffbb26ce65d62c78e2
Dheeraj101590 <thedheerajsharma13@gmail.com>
2026-01-22 10:55:53 -0500

    lab01

---
+++ Week01/q1.py
@@ -0,0 +1,18 @@
+# Sample Coding Questions 01 Week 01
+# Dheeraj# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Original expression:
+# e = a - b ** c // d + a % c
+
+# Fully-Bracketed Version:
+e = (a - ((b ** c) // d)) + (a % c)# Question 4: Formatting
+# Question 5: Common Functions - User Input
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+
+print("Now showing the shop items filtered by age:", userAge)
